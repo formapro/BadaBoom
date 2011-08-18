4c138b158b5e8a2ffaad80b1166611fb100ca34e
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2011-08-18 18:04:19 +0300

    init

---
+++ lib/BadaBoom/Handler.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace BadaBoom;
+
+class Handler
+{
+    
+}
---
+++ tests/BadaBoom/Tests/HandlerTestCase.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace BadaBoom\Tests;
+
+class HandlerTestCase extends \Phpunit_Framework_TestCase
+{
+    
+}
