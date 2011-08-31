1ef866c4e64406a1fd46a538922e113431dc0848
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2011-08-31 14:30:29 +0300

    filter by exception class.

--- src/BadaBoom/ChainNode/Filter/ExceptionClassFilter.php
+++ src/BadaBoom/ChainNode/Filter/ExceptionClassFilter.php
@@ -6,7 +6,41 @@ use BadaBoom\DataHolder\DataHolderInterface;
 
 class ExceptionClassFilter extends AbstractFilterChainNode
 {
+    protected $rules = array();
+
+    public function allow($exceptionClass)
+    {
+        $this->setRule($exceptionClass, true);
+    }
+
+    public function deny($exceptionClass)
+    {
+        $this->setRule($exceptionClass, false);
+    }
+
+    protected function setRule($exceptionClass, $rule)
+    {
+        if (false == \class_exists($exceptionClass)) {
+            throw new \InvalidArgumentException('Class not exists: `'.$exceptionClass.'`');
+        }
+
+        $rc = new \ReflectionClass($exceptionClass);
+        if ('Exception' != $exceptionClass  && false == $rc->isSubclassOf('Exception')) {
+            throw new \InvalidArgumentException('Class `'.$exceptionClass.'` is not a subclass of `Exception`');
+        }
+
+        $this->rules[$exceptionClass] = $rule;
+    }
+
     public function filter(DataHolderInterface $data)
     {
+        $e = $data->get('exception');
+        foreach (\array_reverse($this->rules) as $exceptionClass => $rule) {
+            if ($e instanceof $exceptionClass) {
+                return $rule;
+            }
+        }
+
+        return false;
     }
 }
