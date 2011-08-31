d148e044074c71e4ffb429632e36df7ac6bd7627
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2011-08-31 11:51:36 +0300

    add exception summary provider

---
+++ src/BadaBoom/ChainNode/Provider/ExceptionSummaryProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BadaBoom\ChainNode\Provider;
+
+use BadaBoom\DataHolder\DataHolderInterface;
+
+class ExceptionSummaryProvider extends AbstractProvider
+{
+    protected $sectionName;
+
+    public function __construct($sectionName = 'summary')
+    {
+        $this->sectionName = $sectionName;
+    }
+
+    public function handle(DataHolderInterface $data)
+    {
+        $e = $data->get('exception');
+        if (false == ($e instanceof \Exception)) {
+            return;
+        }
+
+        $data->set($this->sectionName, array(
+            'class' => get_class($e),
+            'code' => $e->getCode(),
+            'message' => $e->getMessage(),
+            'file' => "{$e->getFile()}, Line: {$e->getLine()}",
+        ));
+    }
+}
---
+++ tests/BadaBoom/Tests/ChainNode/Provider/ExceptionSummaryProviderTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace BadaBoom\Tests\ChainNode\Provider;
+
+use BadaBoom\DataHolder\DataHolder;
+use BadaBoom\ChainNode\Provider\ExceptionSummaryProvider;
+
+class ExceptionSummaryProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     * @test
+     */
+    public function shouldBeSubclassOfAbstractProvider()
+    {
+        $rc = new \ReflectionClass('BadaBoom\ChainNode\Provider\ExceptionSummaryProvider');
+        $this->assertTrue($rc->isSubclassOf('BadaBoom\ChainNode\Provider\AbstractProvider'));
+    }
+
+    /**
+     *
+     * @test
+     */
+    public function shouldSetDefaultSectionNameIfNotProvided()
+    {
+        $e = new \Exception('foo');
+
+        $data = new DataHolder();
+        $data->set('exception', $e);
+
+        $provider = new ExceptionSummaryProvider();
+        $provider->handle($data);
+
+        $this->assertTrue($data->has('summary'));
+    }
+
+    /**
+     *
+     * @test
+     */
+    public function shouldUseCustomSectionNameIfProvided()
+    {
+        $e = new \Exception('foo');
+
+        $data = new DataHolder();
+        $data->set('exception', $e);
+
+        $provider = new ExceptionSummaryProvider('barSection');
+        $provider->handle($data);
+
+        $this->assertTrue($data->has('barSection'));
+    }
+
+    /**
+     *
+     * @test
+     */
+    public function shouldFillDataHolderWithExceptionInfo()
+    {
+        $e = new \Exception('foo', 123);
+
+        $data = new DataHolder();
+        $data->set('exception', $e);
+
+        $provider = new ExceptionSummaryProvider();
+        $provider->handle($data);
+
+        $summary = $data->get('summary');
+        $this->assertInternalType('array', $summary);
+
+        $this->assertArrayHasKey('class', $summary);
+        $this->assertArrayHasKey('code', $summary);
+        $this->assertArrayHasKey('message', $summary);
+        $this->assertArrayHasKey('file', $summary);
+
+        $this->assertEquals('Exception', $summary['class']);
+        $this->assertEquals(123, $summary['code']);
+        $this->assertEquals('foo', $summary['message']);
+        $this->assertContains('BadaBoom/Tests/ChainNode/Provider/ExceptionSummaryProviderTest.php', $summary['file']);
+    }
+
+    /**
+     *
+     * @test
+     */
+    public function shouldNotFillDataIfNoExceptionGiven()
+    {
+        $data = new DataHolder();
+        
+        $provider = new ExceptionSummaryProvider();
+        $provider->handle($data);
+
+        $this->assertFalse($data->has('summary'));
+    }
+}
