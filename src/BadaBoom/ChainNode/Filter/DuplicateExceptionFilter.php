0d7666b94dc091fab68d256aec7895f6e67e1a78
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2011-09-07 17:41:27 +0300

    implement duplicate exception filter

--- src/BadaBoom/ChainNode/Filter/DuplicateExceptionFilter.php
+++ src/BadaBoom/ChainNode/Filter/DuplicateExceptionFilter.php
@@ -2,12 +2,39 @@
 
 namespace BadaBoom\ChainNode\Filter;
 
-use BadaBoom\DataHolder\DataHolderInterface;
+use BadaBoom\Adapter\Cache\CacheAdapterInterface;
 
 class DuplicateExceptionFilter extends AbstractFilterChainNode
 {
+    /**
+     * @var BadaBoom\Adapter\Cache\CacheAdapterInterface
+     */
+    protected $cache;
+
+    /**
+     * @var integer
+     */
+    protected $lifeTime;
+
+    public function __construct(CacheAdapterInterface $cache, $lifeTime = 360)
+    {
+        $this->cache = $cache;
+        $this->lifeTime = $lifeTime;
+    }
+
     public function filter(\Exception $exception)
     {
-        
+        $id = md5(
+            get_class($exception) .
+            $exception->getFile() .
+            $exception->getLine());
+//var_dump($id);
+        if ($this->cache->contains($id)) {
+//            var_dump(1);
+            return false;
+        }
+//var_dump(2);
+        $this->cache->save($id, 1, $this->lifeTime);
+        return true;
     }
 }
--- tests/BadaBoom/Tests/ChainNode/Filter/DuplicateExceptionFilterTest.php
+++ tests/BadaBoom/Tests/ChainNode/Filter/DuplicateExceptionFilterTest.php
@@ -2,7 +2,10 @@
 
 namespace BadaBoom\Tests\ChainNode\Filter;
 
-class ExceptionClassFilterTest extends \PHPUnit_Framework_TestCase
+use BadaBoom\Adapter\Cache\ArrayCacheAdapter;
+use BadaBoom\ChainNode\Filter\DuplicateExceptionFilter;
+
+class DuplicateExceptionFilterTest extends \PHPUnit_Framework_TestCase
 {
     /**
      *
@@ -12,5 +15,57 @@ class ExceptionClassFilterTest extends \PHPUnit_Framework_TestCase
     {
         $rc = new \ReflectionClass('BadaBoom\ChainNode\Filter\DuplicateExceptionFilter');
         $this->assertTrue($rc->isSubclassOf('BadaBoom\ChainNode\Filter\AbstractFilterChainNode'));
+    }
+
+    /**
+     * 
+     * @test
+     */
+    public function shouldRequireCacheAdapterAndLifeTimeProvidedInConstructor()
+    {
+        new DuplicateExceptionFilter(new ArrayCacheAdapter, 2000);
+    }
+
+    /**
+     * 
+     * @test
+     */
+    public function shouldPassIfExceptionComesFirstTime()
+    {
+        $filter = new DuplicateExceptionFilter(new ArrayCacheAdapter, 2000);
+
+        $this->assertTrue($filter->filter(new \Exception('foo')));
+    }
+
+    /**
+     *
+     * @test
+     */
+    public function shouldFilterIfExceptionComesForSecondTime()
+    {
+        $filter = new DuplicateExceptionFilter(new ArrayCacheAdapter, 2000);
+
+        $e = new \Exception('foo');
+
+        $this->assertTrue($filter->filter($e));
+        $this->assertFalse($filter->filter($e));
+    }
+
+    /**
+     *
+     * @test
+     */
+    public function shouldPassIfLifeTimeIsOver()
+    {
+        $filter = new DuplicateExceptionFilter(new ArrayCacheAdapter, 1);
+
+        $e = new \Exception('foo');
+
+        $this->assertTrue($filter->filter($e));
+        $this->assertFalse($filter->filter($e));
+
+        sleep(1);
+
+        $this->assertTrue($filter->filter($e));
     }
 }
