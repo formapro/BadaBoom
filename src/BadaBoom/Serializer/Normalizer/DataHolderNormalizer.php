e0551fb25d685368e95b1269e139451050e5ec70
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2011-09-13 11:15:54 +0300

    normalizer draft version

--- src/BadaBoom/Serializer/Normalizer/DataHolderNormalizer.php
+++ src/BadaBoom/Serializer/Normalizer/DataHolderNormalizer.php
@@ -3,27 +3,37 @@
 namespace BadaBoom\Serializer\Normalizer;
 
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Exception\UnsupportedException;
+
+use BadaBoom\DataHolder\DataHolderInterface;
 
 class DataHolderNormalizer implements NormalizerInterface
 {
     public function normalize($object, $format = null)
     {
-        
+        $result = array();
+        foreach ($object as $key => $value) {
+            $result[$key] = $value;
+        }
+
+        return $result;
     }
 
     public function denormalize($data, $class, $format = null)
     {
-        
+
+
+        throw new UnsupportedException('Denormalization is not supported by this normalizer `'.__CLASS__.'`');
     }
 
     public function supportsNormalization($data, $format = null)
     {
-
+        return  $data instanceof DataHolderInterface;
     }
 
     public function supportsDenormalization($data, $type, $format = null)
     {
-
+        return false;
     }
 }
  
--- tests/BadaBoom/Tests/Serializer/Normalizer/DataHolderNormalizerTest.php
+++ tests/BadaBoom/Tests/Serializer/Normalizer/DataHolderNormalizerTest.php
@@ -2,8 +2,52 @@
 
 namespace BadaBoom\Tests\Serializer\Normalizer;
 
+use BadaBoom\Serializer\Normalizer\DataHolderNormalizer;
+use BadaBoom\DataHolder\DataHolder;
+
 class DataHolderNormalizerTest extends \PHPUnit_Framework_TestCase
 {
+    public static function provideSupportedNormalizations()
+    {
+        return array(
+            array(new DataHolder(), 'Should support normalization of DataHolder to all formats'),
+        );
+    }
+
+    public static function provideNotSupportedNormalizations()
+    {
+        return array(
+            array(new \stdClass(),  'Should not support normalization of not DataHolder'),
+            array('foo',            'Should not support normalization of not DataHolder'),
+            array(array(),          'Should not support normalization of not DataHolder'),
+            array(123,              'Should not support normalization of not DataHolder'),
+        );
+    }
+
+    public static function provideNotSupportedDenormalizations()
+    {
+        return array(
+            array(array('foo'), 'stdClass',                       'Should not support any types of denormalization'),
+            array(array('foo'), 'BadaBoom\DataHolder\DataHolder', 'Should not support any types of denormalization'),
+            array('foo',        'BadaBoom\DataHolder\DataHolder', 'Should not support any types of denormalization'),
+            array(123,          'BadaBoom\DataHolder\DataHolder', 'Should not support any types of denormalization'),
+        );
+    }
+
+    public static function provideNormalizationData()
+    {
+        $scalarDataHolder = new DataHolder();
+        $scalarDataHolder->set('foo', 123);
+        $scalarDataHolder->set('bar', 'bar');
+        $scalarDataHolder->set('ololo', 2.3);
+        $scalarArray = array('foo' => 123, 'bar' => 'bar', 'ololo' => 2.3);
+
+        return array(
+            array(new DataHolder(), array(), 'An empty DataHolder should be converted to an empty array'),
+            array($scalarDataHolder, $scalarArray, 'Scalar DataHolder should be converted to scalar array'),
+        );
+    }
+
     /**
      *
      * @test
@@ -12,5 +56,74 @@ class DataHolderNormalizerTest extends \PHPUnit_Framework_TestCase
     {
         $rc = new \ReflectionClass('BadaBoom\Serializer\Normalizer\DataHolderNormalizer');
         $this->assertTrue($rc->implementsInterface('Symfony\Component\Serializer\Normalizer\NormalizerInterface'));
+    }
+
+    /**
+     *
+     * @test
+     *
+     * @dataProvider provideSupportedNormalizations
+     */
+    public function shouldSupportNormalizationOf($data, $failMessage)
+    {
+        $normalizer = new DataHolderNormalizer;
+
+        $this->assertTrue($normalizer->supportsNormalization($data), $failMessage);
+    }
+
+    /**
+     *
+     * @test
+     *
+     * @dataProvider provideNotSupportedNormalizations
+     */
+    public function shouldNotSupportNormalizationOf($data, $failMessage)
+    {
+        $normalizer = new DataHolderNormalizer;
+
+        $this->assertFalse($normalizer->supportsNormalization($data), $failMessage);
+    }
+
+    /**
+     *
+     * @test
+     *
+     * @dataProvider provideNotSupportedDenormalizations
+     */
+    public function shouldNotSupportAnyDenormalizations($data, $type, $failMessage)
+    {
+        $normalizer = new DataHolderNormalizer;
+
+        $this->assertFalse($normalizer->supportsDenormalization($data, $type), $failMessage);
+    }
+
+    /**
+     *
+     * @test
+     *
+     * @dataProvider provideNotSupportedDenormalizations
+     *
+     * @expectedException Symfony\Component\Serializer\Exception\UnsupportedException
+     * @expectedExceptionMessage Denormalization is not supported by this normalizer
+     */
+    public function shouldThrowOnDenormalizeCall($data, $type, $failMessage)
+    {
+        $normalizer = new DataHolderNormalizer;
+
+        $normalizer->denormalize($data, $type);
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider provideNormalizationData
+     */
+    public function shouldNormalizeDataHolderIntoArray($data, $expectedNormalizedData, $failMessage)
+    {
+        $normalizer = new DataHolderNormalizer;
+
+        $actualNormalizedData = $normalizer->normalize($data);
+
+        $this->assertEquals($expectedNormalizedData, $actualNormalizedData, $failMessage);
     }
 }
