a3d4065eff12eaba6334695bc58fe9ff5c714ce5
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2012-01-10 15:24:47 +0200

    [provider] imporve summary provider
    uri - add option it could be url or command line
    code - for errors it will be human readable error code

--- src/BadaBoom/ChainNode/Provider/ExceptionSummaryProvider.php
+++ src/BadaBoom/ChainNode/Provider/ExceptionSummaryProvider.php
@@ -24,11 +24,65 @@ class ExceptionSummaryProvider extends AbstractProvider
     {
         $data->set($this->sectionName, array(
             'class' => get_class($exception),
-            'code' => $exception->getCode(),
+            'uri' => $this->getUri(),
+            'code' => $this->getCode($exception),
             'message' => $exception->getMessage(),
             'file' => "{$exception->getFile()}, Line: {$exception->getLine()}",
         ));
 
         $this->handleNextNode($exception, $data);
+    }
+
+    protected function getUri()
+    {
+        $uri = 'undefined';
+        if (isset($_SERVER['HTTP_HOST'], $_SERVER['REQUEST_URI'])) {
+            $uri = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        } elseif(isset($_SERVER['argv'])) {
+            $uri = implode(' ', $_SERVER['argv']);
+        }
+
+        return $uri;
+    }
+
+    protected function getCode(\Exception $exception)
+    {
+        return $exception instanceof \ErrorException ?
+            $this->getHumanErrorCode($exception->getSeverity()) :
+            $exception->getCode()
+        ;
+    }
+
+    /**
+     *
+     * @param string $name
+     *
+     * @return int|string
+     */
+    public function getHumanErrorCode($errorCode)
+    {
+        $errorCodes = array(
+            'E_ERROR' => E_ERROR,
+            'E_RECOVERABLE_ERROR' => E_RECOVERABLE_ERROR,
+            'E_WARNING' => E_WARNING,
+            'E_PARSE' => E_PARSE,
+            'E_NOTICE' => E_NOTICE,
+            'E_STRICT' => E_STRICT,
+            'E_CORE_ERROR' => E_CORE_ERROR,
+            'E_CORE_WARNING' => E_CORE_WARNING,
+            'E_COMPILE_ERROR' => E_COMPILE_ERROR,
+            'E_COMPILE_WARNING' => E_COMPILE_WARNING,
+            'E_USER_ERROR' => E_USER_ERROR,
+            'E_USER_WARNING' => E_USER_WARNING,
+            'E_USER_NOTICE' => E_USER_NOTICE,
+            'E_ALL' => E_ALL,
+        );
+
+        $humanErrorCode = array_search($errorCode, $errorCodes);
+
+        return false !== $humanErrorCode ?
+            $humanErrorCode :
+            'E_UNKNOWN'
+        ;
     }
 }
