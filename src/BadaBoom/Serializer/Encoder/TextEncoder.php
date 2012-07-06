7be395d4833e00d7a43f4348ad203bafb8e5f277
Maksim Kotlyar <mkotlar@ukr.net>
2012-07-06 10:08:45 +0300

    Update not-approved

--- src/BadaBoom/Serializer/Encoder/TextEncoder.php
+++ src/BadaBoom/Serializer/Encoder/TextEncoder.php
@@ -59,6 +59,6 @@ class TextEncoder implements EncoderInterface
      */
     public function supportsEncoding($format)
     {
-        return 'plain-text' === $format;
+        return 'text' === $format;
     }
 }
